f7356d1bc6cc6fdb93d0a6a355d4a6c7147d92c1
senna07p <matheusynw3@gmail.com>
2026-02-05 15:54:17 -0300

    Sending a new version of design

--- src/App.jsx
+++ src/App.jsx
@@ -2,63 +2,26 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <div className="start-container">
-        <header>
-          <div className="container-title">
-            <span className="title">About Stack</span>
-          </div>
-          <nav className="container-links">
-            <a href="" className="link">
-              Início
-            </a>
-            <a href="" className="link">
-              Sobre
-            </a>
-            <a href="" className="link">
-              Contribuição
-            </a>
-            <a href="" className="link">
-              Contatos
-            </a>
-          </nav>
-          <div className="container-buttons">
-            <button>Começar</button>
-          </div>
-        </header>
-        <div className="container-start">
-          <div className="container-details">
-            <h1 className="title">Crie sua pilha de links personalizada</h1>
-            <p className="desc">
-              Organize todos os seus links em uma bio com design responsivo e
-              recursos avançados, totalmente grátis.
-            </p>
-            <button>Personalize a sua bio</button>
-          </div>
-          <div className="container-banner">
-            <img src="/banner.png" alt="" className="banner" />
-          </div>
-        </div>
-      </div>
-      <div className="container-contribution">
-        <div className="container-details">
-          <h1 className="title">Contribua para o projeto</h1>
+      <div className="home-container">
+        <div className="details-container">
+          <h1 className="title">
+            Crie sua pilha de<br></br>links personalizada
+          </h1>
           <p className="desc">
-            Envie sugestões, críticas ou denúncias e ajude a tornar a
-            experiência dos usuários ainda melhor.
+            Organize todos os seus links em uma bio com design responsivo e
+            recursos avançados, totalmente grátis.
           </p>
-          <button>Fazer uma contribuição</button>
+          <button className="start-button">Personalize a sua bio</button>
         </div>
       </div>
       <footer>
         <span className="copyright-text">
-          &copy; 2026. Design por @sennaynz, licenciado sob Creative Commons
-          Attribution (CC BY). Todos os créditos devem ser mantidos.
+          Todos os direitos reservados &copy; 2026.<br></br>Design por @sennaynz
+          (CC0).
         </span>
-        <button className="top">
-          <i className="fa-solid fa-arrow-up"></i>
-        </button>
       </footer>
     </>
   );
