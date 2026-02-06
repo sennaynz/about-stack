215da8012aa634614ad64a76cd4c3500f6b479f6
senna07p <matheusynw3@gmail.com>
2026-02-06 16:06:11 -0300

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
 import FAQ from "./components/FAQ";
+import banner from "./assets/home.png"
 import "./App.css";
 
 function App() {
-  const [open, setOpen] = useState(false);
   return (
     <>
+      <header>
+        <a href="" className="link">
+          <span className="title">About Stack</span>
+        </a>
+        <nav className="links-container">
+          <a href="" className="link">Home</a>
+          <a href="" className="link">Sobre</a>
+          <a href="" className="link">Começar</a>
+          <a href="" className="link">Contatos</a>
+        </nav>
+      </header>
       <div className="home-container">
         <div className="details-container">
-          <h1 className="title">
-            Crie sua pilha de<br></br>links personalizada
-          </h1>
-          <p className="desc">
-            Organize todos os seus links em uma bio com design responsivo e
-            recursos avançados, totalmente grátis.
-          </p>
-          <button className="start-button">Personalize a sua bio</button>
+          <h1 className="title">Crie sua pilha de<br></br>links personalizada</h1>
+          <p className="desc">Organize todos os seus links em uma bio com design responsivo e recursos avançados, totalmente grátis.</p>
+          <button class="start-button">Personalize a sua bio</button>
+          <img src={banner} alt="" />
         </div>
       </div>
       <div className="faq-container">
@@ -23,11 +30,12 @@ function App() {
         <FAQ />
       </div>
       <footer>
-        <span className="copyright-text">
-          Todos os direitos reservados &copy; 2026.<br></br>Design por @sennaynz
-          (CC0).
-        </span>
-      </footer>
+        <div className="footer-container">
+          <span className="copyright-text">
+            About Stack &copy; Todos os Direitos Reservados.<br></br>Desenvolvido por @sennaynz.
+          </span>
+        </div>
+      </footer >
     </>
   );
 }
