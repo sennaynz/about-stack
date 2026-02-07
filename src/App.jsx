1cc15ffabd16747463aa62c3128060a2f8055e35
senna07p <matheusynw3@gmail.com>
2026-02-06 22:04:33 -0300

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,11 @@ function App() {
           <span className="title">About Stack</span>
         </a>
         <nav className="links-container">
-          <a href="" className="link">Home</a>
-          <a href="" className="link">Sobre</a>
-          <a href="" className="link">Começar</a>
-          <a href="" className="link">Contatos</a>
+          <a href="/home/" className="link">Home</a>
+          <a href="/sobre/" className="link">Sobre</a>
+          <a href="/começar/" className="link">Começar</a>
+          <a href="/contatos/" className="link">Contatos</a>
+          <i class="fa-solid fa-bars"></i>
         </nav>
       </header>
       <div className="home-container">
@@ -24,6 +25,27 @@ function App() {
           <button class="start-button">Personalize a sua bio</button>
           <img src={banner} alt="" />
         </div>
+      </div>
+      <div className="about-container">
+        <div className="details-container">
+          <h1 className="title">Um link. Toda a sua presença online.</h1>
+          <p className="desc">Centralize seus links, destaque seu conteúdo e facilite o acesso do seu público com uma página rápida, personalizável e profissional.</p>
+        </div>
+        <div className="more-container">
+          <div className="box">
+            <h1 className="title">
+              <i className="fa-solid fa-palette"></i> Personalização total
+            </h1>
+            <p className="desc">Edite cores, fotos e links para deixar sua página com a sua identidade.</p>
+          </div>
+          <div className="box">
+            <h1 className="title">
+              <i className="fa-solid fa-bolt"></i> Rápido e prático
+            </h1>
+            <p className="desc">Crie seu link em minutos e compartilhe na bio sem complicação.</p>
+          </div>
+        </div>
+
       </div>
       <div className="faq-container">
         <h1 className="title">Perguntas frequentes</h1>
